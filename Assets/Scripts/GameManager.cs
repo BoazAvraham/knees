23f2ad129f2072cff07a09f551bae563f4ebe3b3
BoazAvraham <boaz.avraham@mail.huji.ac.il>
2019-03-25 18:07:52 +0200

    Running Background

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private float vertExtent, horzExtent;
 
     public static GameManager instance=null;
 
     public float health;
     public Text gameOverText;
-    
+
+    public GameObject background;
+
+    public float screenspeed = 10;
+
+    private float vertExtent, horzExtent;
+    private List<GameObject> walls;
+    private Vector3 wallMovement;
+    private Vector3 wallWidth;
+
     void Awake()
     {
         if (instance == null)
@@ -26,8 +35,42 @@ public class GameManager : MonoBehaviour
     void Start() 
     {
         gameOverText.enabled = false;
+        walls = new List<GameObject>();
+
+        wallMovement = new Vector3(screenspeed,0,0);
+
+        wallWidth = background.transform.GetChild(1).position;
+
+        createWallPool();
+        Debug.Log("num of walls: " + walls.Count);
+        Debug.Log("wall width:"+wallWidth);
     }
 
+    private void createWallPool() {
+        Vector3 pos = wallWidth;
+        pos += 2 * wallWidth;
+        walls.Add(Instantiate(background, pos, Quaternion.identity));
+        pos -= 2 * wallWidth;
+        walls.Add(Instantiate(background,pos,Quaternion.identity));
+        pos -= 2 * wallWidth;
+        walls.Add(Instantiate(background,pos,Quaternion.identity));
+    }
+
+    void Update() {
+        //move walls
+        foreach (GameObject o in walls) {
+            o.transform.position -= wallMovement * Time.deltaTime;
+            //if wall rightboarder out of camera vision
+            if (o.transform.position.x+wallWidth.x < -horzExtent) {
+                Vector3 pos = o.transform.position;
+                pos += 4 * wallWidth;
+                o.transform.position = pos;
+            }
+        }
+
+
+
+    }
 
     public float getVertExtent() { return vertExtent; }
     public float getHorzExtent() { return horzExtent; }
