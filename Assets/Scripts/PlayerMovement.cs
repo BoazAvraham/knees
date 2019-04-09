f1d9d0cf1a54ca38d2d9b7d01ee5437fa3c6432e
TheXino <39231904+TheXino@users.noreply.github.com>
2019-04-09 18:43:51 +0300

    Revert "Meeting"
    This reverts commit ae37397d21057bb63afad86596eb7f53aeb4bc8c.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,38 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 10;
-    public Animator animator;
-    public GameObject slidePoint;
-
 
     private float direction = 0;
     private Vector3 move;
     private float yBound;
-    // Time when the movement started.
-    private float startTime;
 
-    // Total distance between the markers.
-    private float journeyLength;
-
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start() {
         move = new Vector3(0, speed, 0);
         yBound = GameManager.instance.getVertExtent();
 
-
-        // Keep a note of the time the movement started.
-        startTime = Time.time;
-
-        // Calculate the journey length.
-        journeyLength = Vector3.Distance(transform.position, slidePoint.transform.position);
-    
+        
         Debug.Log(yBound);
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        //direction = Input.GetAxis("Vertical");
+        direction = Input.GetAxis("Vertical");
 
         Vector3 movement = move * direction * Time.deltaTime;
 
@@ -45,18 +32,7 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetTrigger("dimple");
         }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            // Distance moved = time * speed.
-            float distCovered = (Time.time - startTime) * speed;
-
-            // Fraction of journey completed = current distance divided by total distance.
-            float fracJourney = distCovered / journeyLength;
-
-            animator.SetTrigger("slide");
-            Vector3.Lerp(transform.position, slidePoint.transform.position,fracJourney);
-        }
-
+        
         Vector3 newPos = transform.position + movement;
         if (newPos.y < yBound && newPos.y > -yBound)
             transform.position = newPos;
