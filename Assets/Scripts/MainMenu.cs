ceb23ae14e5ddd941ee0e66203cfe145e05d215b
BoazAvraham <boaz.avraham@mail.huji.ac.il>
2019-04-26 10:29:05 +0300

    Sound and settings
    KNEESSSSSS

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
 ﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class MainMenu : MonoBehaviour
 {
     public AudioClip click;
+    public AudioMixer audioMixer;
+
     private AudioSource source;
     private int levelToLoad;
+
+
+
+    public GameObject buttonsPanel;
+    public GameObject settingsPanel;
+    public GameObject scoreboardPanel;
+
 
     public void Start()
     {
@@ -16,6 +27,36 @@ public class MainMenu : MonoBehaviour
     public void LoadKneesLevel() {
         source.PlayOneShot(click, 1);
         SceneManager.LoadScene(1);
+    }
+
+
+    public void LoadMainMenu() {
+        settingsPanel.SetActive(false);
+        scoreboardPanel.SetActive(false);
+        buttonsPanel.SetActive(true);
+
+        source.PlayOneShot(click, 1);
+    }
+
+    public void LoadSettingsMenu() {
+        buttonsPanel.SetActive(false);
+        scoreboardPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+
+        source.PlayOneShot(click, 1);
+    }
+
+    public void LoadScoreboardMenu() {
+        buttonsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        scoreboardPanel.SetActive(true);
+
+        source.PlayOneShot(click, 1);
+    }
+
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("Volume",volume);
     }
 
 
